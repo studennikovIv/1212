be33e08c6a28474f4f0e0423027092a4d31c0fef
grant956 <68757554+studennikovIv@users.noreply.github.com>
2024-10-05 00:17:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="psychology">
+    <BrowserRouter basename="1212">
       <App />
     </BrowserRouter>
   </React.StrictMode>
